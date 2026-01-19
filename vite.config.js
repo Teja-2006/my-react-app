5ecebe8ba723fce3d41fb4d997e3ec236a84afa0
Teja-2006 <tejascs289022@gmail.com>
2026-01-19 09:18:07 +0530

    d_c

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/", // Set to '/' since you are using a custom domain at the root
   plugins: [react()],
-  base: "/my-react-app/",
 });
